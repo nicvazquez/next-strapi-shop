aca9571484449a87e1f97a3d7ed6c9f02d6a804d
Nicolas Vazquez <81201032+nicvazquez@users.noreply.github.com>
2022-12-23 17:11:57 -0300

    Card updated

--- next-shop-frontend/pages/index.tsx
+++ next-shop-frontend/pages/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { Input } from "../components/Input";
 import { MainLayout } from "../components/Layouts/MainLayout";
+import { Tag } from "../components/Tag";
 import { Product, Products } from "../interfaces/products";
 import { getProducts } from "../utils";
 
@@ -21,6 +22,7 @@ function HomePage({ products }: Props) {
 		occaecat fugiat aliqua."
 		>
 			<Input
+				className="my-10"
 				state={searchProduct}
 				handleChange={(e) => setSearchProduct(e.target.value)}
 				type="search"
@@ -37,20 +39,29 @@ function HomePage({ products }: Props) {
 					.map((product: Product) => (
 						<li
 							key={product.id}
-							className="block p-6 rounded-lg shadow-lg hover:shadow-md transition bg-white w-full md:w-unset"
+							className="block p-6 rounded-lg shadow-lg hover:shadow-md transition bg-white w-full md:w-72"
 						>
-							<Link href={`/products/${product.id}`} className="flex gap-4">
-								<Image
-									src={`http://127.0.0.1:1337${product.attributes.image.data.attributes.url}`}
-									alt={`${product.attributes.image.data.attributes.alternativeText}`}
-									width={50}
-									height={30}
-								/>
+							<Link
+								href={`/products/${product.id}`}
+								className="flex flex-col gap-4"
+							>
+								<div className="flex gap-4">
+									<Image
+										src={`http://127.0.0.1:1337${product.attributes.image.data.attributes.url}`}
+										alt={`${product.attributes.image.data.attributes.alternativeText}`}
+										width={50}
+										height={30}
+									/>
 
-								<div>
-									<h2 className="font-bold">{product.attributes.title}</h2>
-									<span>{product.attributes.category}</span>
+									<div>
+										<h2 className="font-bold">{product.attributes.title}</h2>
+										<Tag name={product.attributes.category} />
+									</div>
 								</div>
+
+								<p className="text-gray-500">
+									{product.attributes.description.slice(0, 50)}...
+								</p>
 							</Link>
 						</li>
 					))}
